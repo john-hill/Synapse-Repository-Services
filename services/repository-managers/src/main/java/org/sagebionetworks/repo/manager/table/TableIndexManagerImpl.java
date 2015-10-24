981535c34777387124a51232ddf40321aa49810d
john-hill <john.hill@sagebase.org>
2015-10-23 17:00:08 -0700

    with attempt to merge broken rows with newer changes

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableIndexManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/table/TableIndexManagerImpl.java
@@ -125,6 +125,6 @@ public class TableIndexManagerImpl implements TableIndexManager {
 		// delete all tables for this index.
 		tableIndexDao.deleteTable(tableId);
 		tableIndexDao.deleteSecondayTables(tableId);
-	}	
-
+	}
+	
 }

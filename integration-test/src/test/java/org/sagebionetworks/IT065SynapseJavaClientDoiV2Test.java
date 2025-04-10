d7efe4bd399ad7073293309b6b9f70b047cbea9a
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2025-04-10 09:16:34 -0700

    PLFM-8841: Fix integration test

--- integration-test/src/test/java/org/sagebionetworks/IT065SynapseJavaClientDoiV2Test.java
+++ integration-test/src/test/java/org/sagebionetworks/IT065SynapseJavaClientDoiV2Test.java
@@ -111,7 +111,7 @@ public class IT065SynapseJavaClientDoiV2Test {
 		
 		externalDoi = createOrUpdateDoiRetrieveAndValidate(externalDoi);
 		
-		assertEquals(externalDoi, synapse.getDoiAssociation(externalDoi.getPortalId(), externalDoi.getObjectId(), DoiObjectType.PORTAL_RESOURCE, null));
+		assertEquals(externalDoi, synapse.getDoi(externalDoi.getPortalId(), externalDoi.getObjectId(), DoiObjectType.PORTAL_RESOURCE, null));
 		assertEquals("https://myportal.synapse.org/doi?objectId=DATASET.123", synapse.getPortalUrl(externalDoi.getPortalId(), externalDoi.getObjectId(), DoiObjectType.PORTAL_RESOURCE, null));
 		
 	}

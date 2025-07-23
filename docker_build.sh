3e9032d32649b740bae684228c05cf10570f55fd
bhoff <bruce.hoff@sagebase.org>
2025-07-23 15:21:20 -0700

    PLFM-9092 try omitting aws-actions/configure-aws-credentials

--- docker_build.sh
+++ docker_build.sh
@@ -83,12 +83,6 @@ fi
 mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_repository_database_connection_url} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_table_cluster_endpoint_0} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 
-
-echo -------------- DEBUG ---------------
-echo AWS_CREDS
-echo ${AWS_CREDS}
-echo ----------- END DEBUG --------------
-
 # create build container and run build
 docker run --user "$(id -u):$(id -g)" -i --rm --name ${build_container_name} \
 -m 5500M \

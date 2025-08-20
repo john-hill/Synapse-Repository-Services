4f172817bff829efaf08a50dcf7add4fee9306b4
bhoff <bruce.hoff@sagebase.org>
2025-08-19 17:23:33 -0700

    PLFM-9092

--- docker_build.sh
+++ docker_build.sh
@@ -80,7 +80,9 @@ if [ ${SETTINGS_XML} ]; then
 fi
 
 
+echo mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_repository_database_connection_url} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_repository_database_connection_url} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
+echo mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_table_cluster_endpoint_0} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_table_cluster_endpoint_0} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 
 

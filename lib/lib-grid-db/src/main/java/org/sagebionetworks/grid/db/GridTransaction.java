2a6181a970d11e124fa18a3aceeb6abf86870691
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2025-09-08 10:42:43 +0200

    PLFM-9215: Docs

--- lib/lib-grid-db/src/main/java/org/sagebionetworks/grid/db/GridTransaction.java
+++ lib/lib-grid-db/src/main/java/org/sagebionetworks/grid/db/GridTransaction.java
@@ -11,7 +11,13 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Transaction configured to use the gridTransactionManager.
+ * Transaction configured to use an explicit gridTransactionManager. The annotation allows to modify the readOnly attribute.
+ * 
+ * This annotation was introduced due to a bug in Spring when using multiple transaction managers and a mix of class and 
+ * method level Transactional annotations. Theoretically when specifying the transactionManager attribute for the Transactional
+ * annotation at the class level, any method level Transactional annotation should pick up the same transaction manager. 
+ * However, we observed that spring would still give precedence to a "Primary" transaction manager configured in the application 
+ * and in the grid case it wasn't the correct transaction manager.
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)

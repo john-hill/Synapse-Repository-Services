853f64c9621406d7f52a7988f769735aff78d77d
bhoff <bruce.hoff@sagebase.org>
2025-09-15 19:00:37 -0700

    PLFM-9092 reduce sts to 1 hour

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/sts/StsManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/sts/StsManagerImpl.java
@@ -38,7 +38,7 @@ import com.google.common.collect.ImmutableMap;
 
 @Component
 public class StsManagerImpl implements StsManager {
-	static final int DURATION_SECONDS = 12 * 60 * 60; // 12 hours
+	static final int DURATION_SECONDS = 1 * 60 * 60; // 1 hour
 	private static final String POLICY_TEMPLATE_FILENAME = "sts-policy-template.json.vtp";
 
 	// The AWS IAM policy string for the actions the user is allowed to do.

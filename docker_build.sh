7f322ac570285edc436562b8520b47d468aa8882
bhoff <bruce.hoff@sagebase.org>
2025-09-23 15:28:38 -0700

    PLFM-9092: debug echo statements

--- docker_build.sh
+++ docker_build.sh
@@ -84,6 +84,10 @@ mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_repository_d
 mysql -u${rds_user_name} -p${rds_password} -h ${org_sagebionetworks_table_cluster_endpoint_0} -sN -e "DROP DATABASE ${db_name};CREATE DATABASE ${db_name};"
 
 
+# TODO remove next lines
+echo org_sagebionetworks_google_cloud_key ${org_sagebionetworks_google_cloud_key}
+echo length of google cloud key is ${#org_sagebionetworks_google_cloud_key}
+
 # create build container and run build
 docker run --user "$(id -u):$(id -g)" -i --rm --name ${build_container_name} \
 -m 5500M \

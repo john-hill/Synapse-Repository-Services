9f6cc36145c1acdd26d8fd280acc7b390d832cf0
bhoff <bruce.hoff@sagebase.org>
2025-09-25 11:25:33 -0700

    PLFM-9253

--- pomSnapshotToLastGitTag.sh
+++ pomSnapshotToLastGitTag.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-#This script will change the vesion of all pom.xml files from:
+#This script will change the version of all pom.xml files from:
 #<version>develop-SNAPSHOT</version>
 # to
 #<version>GitTag</version>
@@ -9,7 +9,7 @@ newVersion=`git describe --tags`
 echo "Changing all pom.xml to version=$newVersion"
 for f in `find . -name "pom.xml"` ; do
      echo "Changing version of $f"
-     #We are using sed to replaces all 'develop-SNAPSHOT' verions with the new
+     #We are using sed to replaces all 'develop-SNAPSHOT' versions with the new
      sed "s|<version>develop-SNAPSHOT</version>|<version>${newVersion}</version>|g" -i $f
 done
 

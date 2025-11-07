a02b70085001743aa1066eed4ac35c6f05dedc5d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-11-07 15:00:37 -0500

    add comment

--- services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/grid/internal/replica/validation/GridReplicaValidationManagerImpl.java
+++ services/repository-managers/src/main/java/org/sagebionetworks/repo/manager/grid/internal/replica/validation/GridReplicaValidationManagerImpl.java
@@ -136,6 +136,8 @@ public class GridReplicaValidationManagerImpl implements GridReplicaValidationMa
 
 			cleanupValidationResults(validationResults);
 
+			// Always apply the new validation results, even if the value did not change.
+			// The client uses the timestamp to determine if results are up-to-date with its local changes.
 			changes.add(createChange(row, validationResults));
 		}
 

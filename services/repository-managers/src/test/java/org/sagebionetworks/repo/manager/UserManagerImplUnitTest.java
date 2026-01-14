ec2965f1d268e9adacd1fc3d55710c26bcb4aaa1
bhoff <bruce.hoff@sagebase.org>
2026-01-14 10:14:43 -0800

    PLFM-9324

--- services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/UserManagerImplUnitTest.java
+++ services/repository-managers/src/test/java/org/sagebionetworks/repo/manager/UserManagerImplUnitTest.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.repo.manager;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;

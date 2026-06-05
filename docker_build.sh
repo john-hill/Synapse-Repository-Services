a3daca5886c8e8b71b0cf4f32ad41b1b0d2455e3
bhoff <bruce.hoff@sagebase.org>
2026-06-05 11:55:27 -0700

    PLFM-9648

--- docker_build.sh
+++ docker_build.sh
@@ -51,7 +51,8 @@ fi
 
 MVN_GOAL=install
 if [ ${build_deploy} ]; then
-	MVN_GOAL=deploy
+#	MVN_GOAL=deploy
+	MVN_GOAL=verify
 	SETTINGS_XML="<settings><servers><server><id>sagebionetworks</id><username>${artifactory_username}</username><password>${artifactory_password}</password></server></servers></settings>"
 fi
 
